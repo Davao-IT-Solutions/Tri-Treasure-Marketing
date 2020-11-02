381c7c89adb1cb7cdc778938be35f315bc788248
Chester Alan <chesteralantagudin@gmail.com>
2020-11-02 15:06:57 +0800

    Updates

--- components/Header.vue
+++ components/Header.vue
@@ -3,12 +3,6 @@
   <header id="header" class="fixed-top d-flex align-items-center">
     <div class="container d-flex align-items-center">
       <div class="logo mr-auto">
-        <!--<h1 class="text-light">
-          <nuxt-link to="/">
-            <span>Tri-Treasure Marketing</span>
-          </nuxt-link>
-        </h1>-->
-        <!-- Uncomment below if you prefer to use an image logo -->
         <nuxt-link to="/">
           <img src="~/assets/img/logo.png" alt="" class="img-fluid">
         </nuxt-link>
