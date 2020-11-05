649b98c9e22ef5c790baf27ef32ee24c48d53b95
Chester Alan <chesteralantagudin@gmail.com>
2020-11-05 18:56:19 +0800

    Updates

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex align-items-center">
+  <section id="hero" class="d-flex align-items-center section-bg">
     <div class="container">
       <div class="row">
         <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
